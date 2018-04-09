e44cdfafc7b70de834732f6eb3744272221aef19
valePV <va.perez19@hotmail.com>
2018-04-09 11:38:28 -0300

    ex

---
+++ udemy-curso/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class Contador extends Component {
+  constructor() {
+    super()
+    this.state = { contador: 1 }
+    setInterval(() => {
+    }, 1000)
+  }
+
+  render() {
+    return <span>{ this.state.contador }</span>
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p>Primer componente con state</p>
+        <Contador />
+      </div>
+    );
+  }
+}
+
+export default App;
